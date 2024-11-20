1fe0180b8e1e2d49b2bf69db9afa6ffdeaf02acd
Nafeel SM <nafeelsm2018@gmail.com>
2024-11-20 07:05:46 +0530

    databaseSeeder

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -24,8 +24,10 @@ class ProjectFactory extends Factory
             'status' => fake()->
                                 randomElement(['pending', 'in_progress', 'completed']),
             'image_path' => fake()->imageUrl(),
-            'created_at' => 1,
-            'updated_at' => 1
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => time(),
+            'updated_at' => time(),
         ];
     }
 }
